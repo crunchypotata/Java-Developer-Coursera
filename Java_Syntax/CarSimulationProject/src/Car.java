fb4d2cd86d57a019a0e0722d9025309d7fbc50ee
Aleksandra Iakimova <yakimova.cg@gmail.com>
2025-01-26 00:14:43 +0100

    learned "Classes concept"

---
+++ Java_Syntax/CarSimulationProject/src/Car.java
@@ -0,0 +1,9 @@
+public class Car {
+    String make;
+    String model;
+    String color;
+    int year;
+    public String getCarDetails() {
+        return "Make: " + make + "\nModel: " + model + "\nColor: " + color + "\nYear: " + year;
+    }
+}
