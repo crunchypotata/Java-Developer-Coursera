44d8d732ca74766da24ce2304c781fb59868646c
Aleksandra Iakimova <yakimova.cg@gmail.com>
2025-07-08 23:35:55 +0200

    Creating Model

---
+++ Part5-Full-Stack-Web-Development/Module2-Backend/Creating-Model/src/main/java/com/coffeshop/menu/CoffeeshopMenuApplication.java
@@ -0,0 +1,19 @@
+package com.coffeshop.menu;
+
+import com.coffeshop.menu.model.Product;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeshopMenuApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeshopMenuApplication.class, args);
+		Product espresso = new Product(1,"Espresso", 2.3);
+		Product croissant = new Product(1,"Croissant", 3.45);
+
+		System.out.println(espresso);
+		System.out.println(croissant);
+	}
+}
