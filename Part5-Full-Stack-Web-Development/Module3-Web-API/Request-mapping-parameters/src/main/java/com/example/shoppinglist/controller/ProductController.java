9a0a41ccc67082d74c4ba2579d25a0266a3a3de1
Aleksandra Iakimova <yakimova.cg@gmail.com>
2025-08-25 18:25:20 +0200

    learned REST controller

---
+++ Part5-Full-Stack-Web-Development/Module3-Web-API/Request-mapping-parameters/src/main/java/com/example/shoppinglist/controller/ProductController.java
@@ -0,0 +1,66 @@
+package com.example.shoppinglist.controller;
+
+import com.example.shoppinglist.model.Product;
+import com.example.shoppinglist.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Product> addProduct(@ModelAttribute Product product) {
+
+        Product product1 = productService.addProduct(product);
+        if (product1 != null) {
+            return ResponseEntity.ok(product1);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product productDetails) {
+
+        Product updatedProduct = productService.updateProduct(id, productDetails);
+        if (updatedProduct != null) {
+            return ResponseEntity.ok(updatedProduct);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
+        if (productService.deleteProduct(id) != null) {
+            return ResponseEntity.ok("Product deleted with id " + id);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
